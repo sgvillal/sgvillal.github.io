8a8b5cb47af1224b38455e83c84f89d13286f29a
mowolfvillageon <stephanievlobos@gmail.com>
2016-02-04 02:15:49 -0800

    send.php1

---
+++ send.php
@@ -0,0 +1,12 @@
+<?php 
+if(isset($_POST['sub']) 
+{ 
+$Question1 = $_POST['q1']; 
+$Question2 = $_POST['college']; 
+$Question3 = $_POST['subjects']; 
+} 
+else 
+{ 
+header("location:index.html"; 
+} 
+?> 
