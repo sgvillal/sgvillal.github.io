f72945713b9c7b05d1737ebca2e36aab839e1cdf
mowolfvillageon <stephanievlobos@gmail.com>
2016-02-04 02:32:06 -0800

    send.php2

--- send.php
+++ send.php
@@ -1,12 +1,28 @@
 <?php 
-if(isset($_POST['sub']) 
-{ 
-$Question1 = $_POST['q1']; 
-$Question2 = $_POST['college']; 
-$Question3 = $_POST['subjects']; 
+if($_SERVER['REQUEST_METHOD'=="POST") {
+$errors=array(); 
+
+if(empty($_POST["q1"])) {
+	$errors[] = "You forgot to answer a question (1)."
 } 
-else 
-{ 
-header("location:index.html"; 
-} 
+
+if(empty($errors)){
+	if(isset($_POST['sub']) 
+	{ 
+	$Question1 = $_POST['q1']; 
+	$Question2 = $_POST['college']; 
+	$Question3 = $_POST['subjects']; 
+	} 
+	else 
+	{ 
+	header("location:index.html"; 
+	}
+
+}
+e;se {
+foreach($errors as $msg) { echo " - $msg<br/>\m", }
+}
+
+
+}
 ?> 
