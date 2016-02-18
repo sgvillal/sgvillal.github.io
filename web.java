fb49f28fec6503c294d1e1bc4b54d789fd8e7bde
Stephanie Villalobos <stephanievlobos@gmail.com>
2016-02-18 11:37:21 -0800

    commiting

--- web.java
+++ web.java
@@ -4,14 +4,14 @@ import javax.swing.*;
 import java.awt.event.*;
 public class web extends Applet implements ActionListener{
 
-    public static void main(String[] args) {
+  /*  public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
        // javax.swing.SwingUtilities.invokeLater(new Runnable() 
       start s = new start();
       s.createAndShowGUI();
       s.setVisible(true);   
-    }
+    } */
             public void init() {
               setBackground(Color.yellow);
               start s = new start();
